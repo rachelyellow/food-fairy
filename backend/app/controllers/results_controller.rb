4493e3347014c9a3ebfbeb6805824d3df8500864
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-05 23:51:45 +0000

    created answers controller, added logic to create method in results controller

--- backend/app/controllers/results_controller.rb
+++ backend/app/controllers/results_controller.rb
@@ -1,7 +1,13 @@
 class ResultsController < ApplicationController
   def create
+    newResult = Result.new({
+      :customer_id => 1,
+      :quiz_id => 1
+    })
   end
 
   def show
+    @result = Result.where(customer_id: 1, quiz_id: 1)
+    render json: @result
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'answers/create'
+
   get 'results/create'
 
   get 'results/show'

8f503f8fa2243d67a05b13526777f922951ee6df
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-06 18:11:18 +0000

    added function to calculate mode

--- frontend_customer/src/Quiz.js
+++ frontend_customer/src/Quiz.js
@@ -29,19 +29,41 @@ class Quiz extends Component {
   }
   
   nextQuestion = (option) => {
-    if (this.state.activeQuestion < this.state.quizData.questions.length) {
+    if (this.state.activeQuestion < this.state.quizData.questions.length - 1) {
       let optionDishIds = this.logAnswer(option)
       this.setState({
         activeQuestion: this.state.activeQuestion + 1,
         answerLog: this.state.answerLog.concat(optionDishIds)
       })
-    } 
+    } else {
+      console.log('calculating dish preference..')
+      console.log('redirecting to results page..')
+    }
   }
 
   logAnswer = (option) => {
     let optionDishes = [];
     option.dishes.forEach((dish) => optionDishes.push(dish.id))
     return optionDishes;
+  }
+
+  calculateDishPreference = () => {
+    const dishPreferences = this.state.answerLog
+    const dishReccomendation = this.mostFrequent(dishPreferences)
+    console.log(dishReccomendation)
+    // axios
+  }
+
+  mostFrequent = (dishIdArr) => {
+    let count = {}
+    dishIdArr.forEach(function (dishId) {
+      if (count[dishId]) {
+        count[dishId] += 1
+      } else {
+        count[dishId] = 1
+      }
+    })
+    return count
   }
 
   render() {

418182b1a6280b91c30fc4800d311b9d7dbe64e5
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-03-07 10:45:13 -0500

    renamed results to rewards

--- frontend_customer/src/App.js
+++ frontend_customer/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Map from './Map.js'
 import NavBar from './NavBar.js'
 import Quiz from './Quiz';
-import Results from './results.js'
+import Rewards from './Rewards.js'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Switch } from 'react-router'
 
@@ -27,7 +27,7 @@ class App extends Component {
         <Switch>
         <Route path="/maps" component={Map} />
         <Route path="/restauant/:restaurant_id/quiz/:quiz_id" component={Quiz} />
-        <Route path="/rewards" component={Results} />
+        <Route path="/rewards" component={Rewards} />
         </Switch>
       </div>
     </Router>

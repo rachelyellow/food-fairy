b201ba627a3a231b178008a750174dae81361393
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-07 16:36:18 +0000

    added status bar component, included in app.js to show on all pages

--- frontend_customer/src/App.js
+++ frontend_customer/src/App.js
@@ -3,34 +3,34 @@ import Map from './Map.js'
 import NavBar from './NavBar.js'
 import Quiz from './Quiz';
 import Results from './results.js'
+import Statusbar from "./Statusbar.js";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Switch } from 'react-router'
 
 
 class App extends Component {
-
   render() {
     return (
-
-    <div className="App">
-    <Router>
-      <div>
-        <ul>  
-          <li>
-          <Link to="/maps">Maps</Link>
-          </li>
-        <li>
-        <Link to="/rewards">Rewards</Link>
-        </li>
-        </ul>
-        <hr />
-        <Switch>
-        <Route path="/maps" component={Map} />
-        <Route path="/restauant/:restaurant_id/quiz/:quiz_id" component={Quiz} />
-        <Route path="/rewards" component={Results} />
-        </Switch>
-      </div>
-    </Router>
+      <div className="App">
+        <Statusbar />
+        <Router>
+          <div>
+            <ul>  
+              <li>
+              <Link to="/maps">Maps</Link>
+              </li>
+              <li>
+              <Link to="/rewards">Rewards</Link>
+              </li>
+            </ul>
+            <hr />
+            <Switch>
+              <Route path="/maps" component={Map} />
+              <Route path="/restauant/:restaurant_id/quiz/:quiz_id" component={Quiz} />
+              <Route path="/rewards" component={Results} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ frontend_customer/src/Statusbar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Statusbar extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Anisa Tahlil</p><button>Log Out</button>
+      </div>
+    );
+  }
+}
+export default Statusbar;

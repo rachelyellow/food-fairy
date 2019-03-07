2418001876f8971d5bf24f48cf745229d34b82ea
GaganLal <lalgagan@outlook.com>
2019-03-07 20:39:56 +0000

    edited quiz controller route to have created method and added has many questions relationship

--- backend/app/controllers/quizzes_controller.rb
+++ backend/app/controllers/quizzes_controller.rb
@@ -5,5 +5,23 @@ class QuizzesController < ApplicationController
              render :json => {:questions => @questions.as_json(include: {options: {include: :dishes} }),
              }     
     end
+
+    def create
+        quiz = Quiz.create(restaurant_id: params["restaurant_id"] )
+        3.times do |n|
+            question = quiz.questions.new(inquiry: params["question[#{n}][name]"])
+            question.save
+
+            3.times do |j|
+               option = question.options.new(name: params["question[#{n}][options][#{j}]"])
+               option.save
+
+               3.times do |k|
+                dish = option.dishes.new(name: params["question[#{n}][options][#{j}][dish][#{k}]"])
+                dish.save
+               end
+            end
+        end
+    end 
 end
 

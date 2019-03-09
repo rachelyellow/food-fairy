b67f956477c10749c4f8ff985b5f7af2cdbe0066
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-09 19:25:43 +0000

    added table template to dish page

--- frontend/src/Dishes.js
+++ frontend/src/Dishes.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table'
 
 class Dishes extends Component {
 
@@ -6,7 +7,31 @@ class Dishes extends Component {
   render() {
     return(
 
-        <h1>Hello dishes form</h1>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+          </tr>
+        </tbody>
+      </Table>
 
     );
   }

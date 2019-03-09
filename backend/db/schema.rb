18578a5a8bb47afbc411fa9abc423f35210cc595
GaganLal <lalgagan@outlook.com>
2019-03-09 00:04:42 +0000

    added column in table and seeded addresses to restaurants

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306191526) do
+ActiveRecord::Schema.define(version: 20190308230326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20190306191526) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "image"
+    t.string "address"
   end
 
   create_table "results", force: :cascade do |t|

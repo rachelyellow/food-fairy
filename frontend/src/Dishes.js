5adc99aa5bebcd6bad466df6f9ae4df8e02e545a
GaganLal <lalgagan@outlook.com>
2019-03-09 23:16:07 +0000

    fixed dishes controller naming and added index route

--- frontend/src/Dishes.js
+++ frontend/src/Dishes.js
@@ -1,51 +1,77 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table'
+import axios from 'axios'; 
 
 class Dishes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: null
+    }
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //console.log(event.target.elements.question.value)
+
+    const data = {}
+
+    for (let entry of new FormData(event.target).entries()) {
+        data[entry[0]] = entry[1]
+    }
+
+    console.log("for loop data", data)
+    axios.post('http://localhost:3000/restaurants/2/dishes',
+      data
+     )
+  }
+  
 
 
   render() {
     return (
-
-      <form>
+      <div>
+      <form onSubmit={this.handleSubmit}>
         <Table striped bordered hover >
           <thead>
             <tr>
               <th>#</th>
-              <th>Name</th>
+              <th>Dish</th>
               <th>Image Link</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>1</td>
-              <td><input type="text" name="restaurant[2]dish[1]"/></td>
-              <td><input type="text" name="restaurant[2][dishimg][1]"/></td>
+              <td><input type="text" name="dish[1]"/></td>
+              <td><input type="text" name="dish[1][img][1]"/></td>
             </tr>
             <tr>
               <td>2</td>
-              <td><input type="text" name="restaurant[2]dish[2]"/></td>
-              <td><input type="text" name="restaurant[2][dishimg][2]"/></td>
+              <td><input type="text" name="dish[2]"/></td>
+              <td><input type="text" name="dish[2][img][2]"/></td>
             </tr>
             <tr>
               <td>3</td>
-              <td><input type="text" name="restaurant[2]dish[3]"/></td>
-              <td><input type="text" name="restaurant[2][dish][1]"/></td>
+              <td><input type="text" name="dish[3]"/></td>
+              <td><input type="text" name="dish[3][img][3]"/></td>
             </tr>
             <tr>
               <td>4</td>
-              <td><input type="text" name="restaurant[2]dish[4]"/></td>
-              <td><input type="text" name="restaurant[2][dishimg][1]"/></td>
+              <td><input type="text" name="dish[4]"/></td>
+              <td><input type="text" name="dish[4][img][4]"/></td>
             </tr>
             <tr>
               <td>5</td>
-              <td><input type="text" name="restaurant[2]dish[5]"/></td>
-              <td><input type="text" name="restaurant[2][dishimg][1]"/></td>
+              <td><input type="text" name="dish[5]"/></td>
+              <td><input type="text" name="dish[5][img][5]"/></td>
             </tr>
           </tbody>
         </Table>
         <button type="submit"> Submit </button>
       </form>
+      </div>
 
     );
   }

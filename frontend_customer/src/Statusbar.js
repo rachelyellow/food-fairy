4c3fb0440046be8e3c59897639c61f95d2657d3e
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-10 17:37:47 +0000

    added logo to status bar

--- frontend_customer/src/Statusbar.js
+++ frontend_customer/src/Statusbar.js
@@ -4,9 +4,10 @@ class Statusbar extends Component {
   render() {
     return (
       <div style={{display:'flex', backgroundColor:'#230f04', height:'4em'}} >
-        <h4 style={{flex: '0 90%', color:'white', padding:'.75em'}} >Anisa Tahlil</h4>
+        <h3 style={{flex: '0 90%', color:'white', padding:'.75em'}} >Food Fairy 🧚</h3>
+        <h4 style={{flex: '0 15%', color:'white', padding:'0.8em 0em', width:'150px'}} >Anisa Tahlil</h4>
         <a href="http://localhost:3001/login" style={{padding:'1em'}} >
-          <button style={{borderRadius:'5px', padding:'5px 8px'}} >Logout</button>
+          <button style={{borderRadius:'5px', padding:'5px 8px', marginRight:'2em'}} >Logout</button>
         </a>
       </div>
     );

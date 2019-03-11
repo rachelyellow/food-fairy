66b1ad66f2d3e065a904e67ea76db2ded7973dc9
Rachel Wong <rachelwong.ly@gmail.com>
2019-03-11 20:24:57 +0000

    changed seed data to be closer to demo location, arranged maps page and changed starting location

--- frontend_customer/src/Map.js
+++ frontend_customer/src/Map.js
@@ -7,8 +7,8 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const mapStyles = {
   width: '80%',
-  height: '100%',
-
+  height: '70%',
+  marginLeft: '9em'
 };
 
 const imageStyle = {
@@ -18,144 +18,145 @@ const imageStyle = {
 }
 
 export class MapContainer extends Component {
-    state = {
-        showingInfoWindow: false,  //Hides or the shows the infoWindow
-        activeMarker: {},          //Shows the active marker upon click
-        selectedPlace: {},   //Shows the infoWindow to the selected place upon a marker
-        restaurants : [],
-        activeUser: 1,
-        quizResults: [],
-        completedQuizzes: [],
-        //RESTAURANT IDs THAT HAVE CREATED A QUIZ
-        availableQuizzes: []
-      };
-
-    //   After the component is mounted, fetch the data from the backend
-    componentDidMount() {
-      axios.get("http://localhost:3000/restaurants")
-      .then(response => {
-        this.setState({restaurants: response.data})
-      })
-      .catch(error => console.log(error))
-
-      axios.get('/results')
-      .then(response => {
-        this.setState({ quizResults: response.data.results }, this.getResultsForUser)
-      })
-      .catch(error => console.log(error))
-
-      axios.get('/quizzes')
-      .then(response => {
-        response.data.quizzes.map((quiz) => this.setState({availableQuizzes: this.state.availableQuizzes.concat(quiz.restaurant_id) }))
-      })
-    }
-
-    onMarkerClick = (props, marker, e) =>
-      this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true
-    });
-
-    onClose = props => {
-      if (this.state.showingInfoWindow) {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        });
-      }
+  state = {
+      showingInfoWindow: false,  //Hides or the shows the infoWindow
+      activeMarker: {},          //Shows the active marker upon click
+      selectedPlace: {},   //Shows the infoWindow to the selected place upon a marker
+      restaurants : [],
+      activeUser: 1,
+      quizResults: [],
+      completedQuizzes: [],
+      //RESTAURANT IDs THAT HAVE CREATED A QUIZ
+      availableQuizzes: []
     };
 
-    getResultsForUser = () => {
-      this.state.quizResults.map((result) => {
-        if (result.customer_id === this.state.activeUser) {
-          //STILL ASSUMING QUIZ ID === RESTO ID
-          this.setState({ completedQuizzes: this.state.completedQuizzes.concat(result.restaurant.id) })
-        }
-      return true
-      })
-    }
-  
-    render() {
-        let to = `/restaurant/${this.state.selectedPlace.id}/quiz/${this.state.selectedPlace.id}`
-        return (
-          <div>
-          <Statusbar/>
-          <NavBar/>
-          <Map
-            google={this.props.google}
-            zoom={16}
-            style={mapStyles}
-            initialCenter={{
-             lat: 43.644345,
-             lng: -79.402150
-            }}
-          >
-          { this.state.restaurants.map((restaurant) => {
+  //   After the component is mounted, fetch the data from the backend
+  componentDidMount() {
+    axios.get("http://localhost:3000/restaurants")
+    .then(response => {
+      this.setState({restaurants: response.data})
+    })
+    .catch(error => console.log(error))
 
-            return (
-              <Marker
-                key={restaurant.id}
-                onClick={this.onMarkerClick}
-                name= {restaurant.name} 
-                id = {restaurant.id}
-                image = {restaurant.image}
-                streetAddress = {restaurant.address}
-                description = {restaurant.description}
-                position = {
-                  {
-                    lat : restaurant.latitude,
-                    lng : restaurant.longitude
-                  }
-                }
-              />
-            ) 
-          }) }
-          
-            <InfoWindow
-            marker={this.state.activeMarker} 
-            visible={this.state.showingInfoWindow} 
-            onClose={this.onClose} >
-              <div>
-                <div>
-                  <h4>{this.state.selectedPlace.name}</h4>
-                  <h6>{this.state.selectedPlace.streetAddress}</h6>
-                  <img style={imageStyle} alt="" src={this.state.selectedPlace.image}/>
-                  <br/>
-                  <i>{this.state.selectedPlace.description}</i>
-                </div>
-                {this.state.activeMarker && !this.state.availableQuizzes.includes(this.state.activeMarker.id) && 
-                <div style={{paddingTop:'10px'}} >
-                  There is currently no quiz for this restaurant.
-                  <br/>
-                  Please check back at a later time.
-                </div>
-                }
-                {this.state.activeMarker && !this.state.completedQuizzes.includes(this.state.activeMarker.id) && this.state.availableQuizzes.includes(this.state.activeMarker.id) &&
-                  <div style={{paddingTop:'10px'}} >
-                    <Router>
-                      <Link to={to}>Take Quiz</Link>
-                    </Router>
-                  </div>
-                }
-                {this.state.activeMarker && this.state.completedQuizzes.includes(this.state.activeMarker.id) && 
-                <div style={{padding:'10px 0 0'}} >
-                    Oops, Seems you've already completed this quiz!
-                    <br/>
-                    Please check
-                    <Router>
-                      <Link to={'/rewards'}> MyRewards </Link>
-                    </Router>
-                    to view your rewards.
-                </div>
-                }
-              </div>
-            </InfoWindow>
-          </Map>
-          </div>
-        );
-      }
+    axios.get('/results')
+    .then(response => {
+      this.setState({ quizResults: response.data.results }, this.getResultsForUser)
+    })
+    .catch(error => console.log(error))
+
+    axios.get('/quizzes')
+    .then(response => {
+      response.data.quizzes.map((quiz) => this.setState({availableQuizzes: this.state.availableQuizzes.concat(quiz.restaurant_id) }))
+    })
+  }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+  });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
     }
+  };
+
+  getResultsForUser = () => {
+    this.state.quizResults.map((result) => {
+      if (result.customer_id === this.state.activeUser) {
+        //STILL ASSUMING QUIZ ID === RESTO ID
+        this.setState({ completedQuizzes: this.state.completedQuizzes.concat(result.restaurant.id) })
+      }
+    return true
+    })
+  }
+
+  render() {
+      let to = `/restaurant/${this.state.selectedPlace.id}/quiz/${this.state.selectedPlace.id}`
+      return (
+        <div>
+        <Statusbar/>
+        <NavBar/>
+        <h2 style={{ color:'white', padding:'1em', marginLeft:'5em' }} >Choose a restaurant to start earning rewards!</h2>
+        <Map
+          google={this.props.google}
+          zoom={17}
+          style={mapStyles}
+          initialCenter={{
+            lat: 43.6433,
+            lng: -79.3994
+          }}
+        >
+        { this.state.restaurants.map((restaurant) => {
+
+          return (
+            <Marker
+              key={restaurant.id}
+              onClick={this.onMarkerClick}
+              name= {restaurant.name} 
+              id = {restaurant.id}
+              image = {restaurant.image}
+              streetAddress = {restaurant.address}
+              description = {restaurant.description}
+              position = {
+                {
+                  lat : restaurant.latitude,
+                  lng : restaurant.longitude
+                }
+              }
+            />
+          ) 
+        }) }
+        
+          <InfoWindow
+          marker={this.state.activeMarker} 
+          visible={this.state.showingInfoWindow} 
+          onClose={this.onClose} >
+            <div>
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+                <h6>{this.state.selectedPlace.streetAddress}</h6>
+                <img style={imageStyle} alt="" src={this.state.selectedPlace.image}/>
+                <br/>
+                <i>{this.state.selectedPlace.description}</i>
+              </div>
+              {this.state.activeMarker && !this.state.availableQuizzes.includes(this.state.activeMarker.id) && 
+              <div style={{paddingTop:'10px'}} >
+                There is currently no quiz for this restaurant.
+                <br/>
+                Please check back at a later time.
+              </div>
+              }
+              {this.state.activeMarker && !this.state.completedQuizzes.includes(this.state.activeMarker.id) && this.state.availableQuizzes.includes(this.state.activeMarker.id) &&
+                <div style={{paddingTop:'10px'}} >
+                  <Router>
+                    <Link to={to}>Take Quiz</Link>
+                  </Router>
+                </div>
+              }
+              {this.state.activeMarker && this.state.completedQuizzes.includes(this.state.activeMarker.id) && 
+              <div style={{padding:'10px 0 0'}} >
+                  Oops, Seems you've already completed this quiz!
+                  <br/>
+                  Please check
+                  <Router>
+                    <Link to={'/rewards'}> MyRewards </Link>
+                  </Router>
+                  to view your rewards.
+              </div>
+              }
+            </div>
+          </InfoWindow>
+        </Map>
+        </div>
+      );
+    }
+  }
     
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDF0n1daTVqgT7582-gLCO-GOsUgsF2-LQ'
